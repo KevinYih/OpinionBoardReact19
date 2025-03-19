8c7e730455c25289039a1a09b32a0681998343bd
keviny <kevinyih2022@gmail.com>
2025-03-19 14:38:52 -0700

    initialize opinion form react 19

---
+++ src/components/NewOpinion.jsx
@@ -0,0 +1,51 @@
+export function NewOpinion() {
+
+  //when submitted, the f shareOpinionAction will be called.
+  function shareOpinionAction(formData) {
+    const userName = formData.get("userName");
+    const title = formData.get("title");
+    const body = formData.get("body");
+
+    let errors = [];
+
+    if (title.trim().length < 5) {
+      errors.push('Title must be at least five characters long.');
+    }
+
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push('Opinion must be between 10 and 300 characters long.');
+    }
+
+    if (!userName.trim()) {
+      errors.push('Please provide your name.');
+    }
+
+  }
+
+  return (
+    <div id="new-opinion">
+      <h2>Share your opinion!</h2>
+      <form action={shareOpinionAction}>
+        <div className="control-row">
+          <p className="control">
+            <label htmlFor="userName">Your Name</label>
+            <input type="text" id="userName" name="userName" />
+          </p>
+
+          <p className="control">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" />
+          </p>
+        </div>
+        <p className="control">
+          <label htmlFor="body">Your Opinion</label>
+          <textarea id="body" name="body" rows={5}></textarea>
+        </p>
+
+        <p className="actions">
+          <button type="submit">Submit</button>
+        </p>
+      </form>
+    </div>
+  );
+}
